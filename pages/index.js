76ba0f1ed0f5b9a1dfbcc354e0056604a2f45b2a
zayarwin <zayarwin7751@gmail.com>
2022-02-18 14:02:57 +0630

    nagivate-programmatically

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,25 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function Home() {
+function Home() {
+  const router = useRouter();
+  const clickHandler = () => {
+    console.log("Ordering item");
+    router.push("/product");
+  };
   return (
     <>
       <h1>Home Page</h1>
       <Link href='/product'>
         <a>Product</a>
       </Link>
+      <br />
+      <button onClick={clickHandler}>
+        Oreder items
+      </button>
     </>
   );
 }
+export default Home;
