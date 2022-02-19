62bb84aae98baea065785de55d8c25afb46ccdf0
zayarwin <zayarwin7751@gmail.com>
2022-02-19 15:45:33 +0630

    final

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,13 +2,41 @@ import { useState } from "react";
 
 const CommentsList = () => {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState(null);
   const loadCommentsHandler = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
   };
+
+  const submitComment = async () => {
+    const response = await fetch(
+      "/api/comments",
+      {
+        method: "POST",
+        body: JSON.stringify({ comment }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    setComments(data);
+    setComment("");
+  };
+
   return (
     <>
+      <input
+        type='text'
+        value={comment}
+        onChange={(e) =>
+          setComment(e.target.value)
+        }
+      />
+      <button onClick={submitComment}>
+        Submit comment
+      </button>
       <button onClick={loadCommentsHandler}>
         Load Comments
       </button>
